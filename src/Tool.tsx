2d2e8d61ecc284138a6420f8e14117add70f82dc
bobeenlee <globaldev@naver.com>
2022-06-30 03:33:17 +0900

    feat: persist darkmode button using localStorage

--- src/Tool.tsx
+++ src/Tool.tsx
@@ -1,17 +1,46 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useGlobals } from "@storybook/api";
 import { Icons, IconButton } from "@storybook/components";
 import { TOOL_ID } from "./constants";
+import { storage as localStorage } from "./localStorage";
 
 export const Tool = () => {
-  const [{ darkMode }, updateGlobals] = useGlobals();
+  const [{ darkMode = localStorage().getDarkMode() }, updateGlobals] = useGlobals();
 
-  const toggleDarkMode = useCallback(
-    () =>
-      updateGlobals({
-        darkMode: !Boolean(darkMode),
-      }),
-    [darkMode]
+  const toggleDarkMode = useCallback(() => {
+    updateGlobals({
+      darkMode: !Boolean(darkMode),
+    });
+    localStorage().setDarkMode(!Boolean(darkMode));
+  }, [darkMode]);
+
+  const DarkMode = (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path fill="none" d="M0 0h24v24H0V0z"></path>
+      <path d="M14 4c.34 0 .68.02 1.01.07C13.1 6.23 12 9.05 12 12s1.1 5.77 3.01 7.93c-.33.05-.67.07-1.01.07-4.41 0-8-3.59-8-8s3.59-8 8-8m0-2C8.48 2 4 6.48 4 12s4.48 10 10 10c1.82 0 3.53-.5 5-1.35-2.99-1.73-5-4.95-5-8.65s2.01-6.92 5-8.65A9.973 9.973 0 0014 2z"></path>
+    </svg>
+  );
+  const LightMode = (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path fill="none" d="M0 0h24v24H0z"></path>
+      <path d="M12 9c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3m0-2c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zM2 13h2c.55 0 1-.45 1-1s-.45-1-1-1H2c-.55 0-1 .45-1 1s.45 1 1 1zm18 0h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1zM11 2v2c0 .55.45 1 1 1s1-.45 1-1V2c0-.55-.45-1-1-1s-1 .45-1 1zm0 18v2c0 .55.45 1 1 1s1-.45 1-1v-2c0-.55-.45-1-1-1s-1 .45-1 1zM5.99 4.58a.996.996 0 00-1.41 0 .996.996 0 000 1.41l1.06 1.06c.39.39 1.03.39 1.41 0s.39-1.03 0-1.41L5.99 4.58zm12.37 12.37a.996.996 0 00-1.41 0 .996.996 0 000 1.41l1.06 1.06c.39.39 1.03.39 1.41 0a.996.996 0 000-1.41l-1.06-1.06zm1.06-10.96a.996.996 0 000-1.41.996.996 0 00-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06zM7.05 18.36a.996.996 0 000-1.41.996.996 0 00-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06z"></path>
+    </svg>
   );
 
   return (
@@ -21,7 +50,7 @@ export const Tool = () => {
       title="Enable dark mode"
       onClick={toggleDarkMode}
     >
-      {darkMode ? <Icons icon="heart" /> : <Icons icon="hearthollow" />}
+      {darkMode ? LightMode : DarkMode}
     </IconButton>
   );
 };
